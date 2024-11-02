1df29f81047a8dc0fb11deedb10071229f27d5c7
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-11-02 10:30:45 +0100

    added value to inputs in edit.blade.php

--- resources/views/movies/edit.blade.php
+++ resources/views/movies/edit.blade.php
@@ -13,27 +13,27 @@
 
             <div class="col-6 m-3">
                 <label for="film-name">Inserisci Titolo</label>
-                <input class="form-control" type="text" aria-label="default input example" id="film-name" name="title">
+                <input class="form-control" type="text" aria-label="default input example" value="{{ $movie->title }}" id="film-name" name="title">
             </div>
 
             <div class="col-6 m-3">
                 <label for="film-original-name">Inserisci Titolo Originale</label>
-                <input class="form-control" type="text" aria-label="default input example" id="film-original-name" name="original_title">
+                <input class="form-control" type="text" aria-label="default input example" value="{{ $movie->original_title }}" id="film-original-name" name="original_title">
             </div>
 
             <div class="col-6 m-3">
                 <label for="film-nationality">Inserisci la nazionalità</label>
-                <input class="form-control" type="text" aria-label="default input example" id="film-nationality" name="nationality">
+                <input class="form-control" type="text" aria-label="default input example" value="{{ $movie->nationality }}" id="film-nationality" name="nationality">
             </div>
 
             <div class="col-6 m-3 mb-4">
                 <label for="film-date">Inserisci la data</label>
-                <input class="form-control" type="text" aria-label="default input example" id="film-date" name="date">
+                <input class="form-control" type="text" aria-label="default input example" value="{{ $movie->date }}" id="film-date" name="date">
             </div>
 
             <div class="col-6 m-3 mb-4">
                 <label for="film-vote">Inserisci il voto</label>
-                <input class="form-control" type="text" aria-label="default input example" id="film-vote" name="vote">
+                <input class="form-control" type="text" aria-label="default input example" value="{{ $movie->vote }}" id="film-vote" name="vote">
             </div>
 
             <div class="text-center">
